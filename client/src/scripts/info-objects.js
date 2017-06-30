83e9e227b9e1a2debb162590ed801ca83b10a8a8
Dinesh Shetye <dinesh.shetye@lexisnexis.com>
2017-06-30 11:02:30 -0400

    Add a common styling for all dialog boxes (#88)

--- client/src/scripts/info-objects.js
+++ client/src/scripts/info-objects.js
@@ -3,39 +3,6 @@ function Plugin(id, label, category) {
 	this.label = label;
 	this.category = category;
 }
-/*
-	Function for inline editing of 'projecttitle' field.
-*/
-function editProjectTitleHandler() {
-	var myDiv = document.getElementById('projecttitle');
-	myDiv.addEventListener('dblclick', function (e) {
-		e.stopPropagation();
-		var currentElement = myDiv;
-		var value = myDiv.innerHTML;
-		updateVal(currentElement, value);
-	});
-
-	function updateVal(currentElement, value) {
-		currentElement.innerHTML = '<input id="thVal" type="text" size="50" value="' + value + '" />';
-		var newEle = document.getElementById('thVal');
-		newEle.focus();
-		newEle.addEventListener('keyup', function (e) {
-			if (e.keyCode == 13) {
-				currentElement.innerHTML = e.target.value;
-			}
-		});
-
-		document.onclick = myClickHandler;
-		function myClickHandler() {
-			if (document.getElementById('thVal') != null) {
-				var newEle = document.getElementById('thVal').value;
-				currentElement.innerHTML = newEle;
-				var infoBox = document.querySelector('#infobox');
-				infoBox.properties.title = newEle;
-			}
-		}
-	}
-}
 
 function hostReachable(eclIP, hpccuser, password) {
 	var promise = new Promise(function (resolve, reject) {
